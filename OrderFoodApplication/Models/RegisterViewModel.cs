da43253c929e98dcf1ac44e43581b1df24c1fe97
Rida702 <ai22c0rida@gmail.com>
2024-01-06 10:55:39 +0500

    Added Registration

---
+++ OrderFoodApplication/Models/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderFoodApplication.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public int Name { get; set; }
+        [Required]
+        public int Address { get; set; }
+        [Required, EmailAddress]
+        public int Email { get; set; }
+        [Required]
+        public int Password { get; set; }
+    }
+}
